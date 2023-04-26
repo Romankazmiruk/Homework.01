ea002274e78bc9935ea77fe565d6f08414a42751
Roma <roman_kazmiruk@mail.ru>
2023-04-26 07:07:08 +0300

    Homewor done

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+
+
+// РЕШЕНИЕ:
+
+// Console.WriteLine("Input first number:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if(num1 > num2)
+// {
+//     Console.WriteLine($"Число {num1} больше, а число {num2} меньше");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {num2} больше, а число {num1} меньше");
+// }
+
+
+
+// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+
+
+// РЕШЕНИЕ
+
+// Console.WriteLine("Input first number:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number:");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+// int max = num1;
+// if (max < num2)
+// {
+// max = num2;
+// }
+// if (max < num3)
+// {
+// max = num3;
+// }
+// Console.WriteLine($"Максимальное число равно {max}");
+
+
+
+// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+
+
+// РЕШЕНИЕ
+
+// Console.WriteLine("Input 3-digit number:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// if(n % 2 == 0) Console.WriteLine($"Число четное");
+// Console.WriteLine($"Число нечетное");
+
+
+
+// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+
+
+// РЕШЕНИЕ
+
+// Console.WriteLine("Input number:");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// if(N<0) Console.WriteLine("invalid input N");
+
+// int count = 1;
+// while(count <= N)
+// {
+
+//     if(count % 2 == 0)
+//     Console.Write(count+" ");
+//     count++;
+// }
+
+
